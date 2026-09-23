ca95098ef7939908309277bf1500271987dd343f
harshBh59 <bhargavharshvardhan95@gmail.com>
2026-09-23 13:46:16 +0530

    Time: 5 ms (23.89%) | Memory: 18 MB (77.00%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+        struct cmp{
+            bool operator()(pair<int,int>&a,pair<int,int>&b){
+                return a.first>b.first;
+                
+               }
+               
+            };
+        vector<int> topKFrequent(vector<int>& nums, int k) {
+        int n=nums.size();
+            unordered_map<int,int>f;
+            for(int i=0;i<n;i++){
+                f[nums[i]]++;
+            }
+            priority_queue<pair<int,int>,vector<pair<int,int>>,cmp>pq;
+            for(auto i:f){
+                int element=i.first;
+                int freq=i.second;
+            
+            pair<int,int>curr={freq,element};
+            pq.push(curr);
+            if(pq.size()>k){
+                pq.pop();
+            }
+            }
+        vector<int>res;
+        while(!pq.empty()){
+            res.push_back(pq.top().second);
+            pq.pop();
+        }
+        return res;
+
+    }
+};
